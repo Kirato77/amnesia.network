d8c0f294c5deb6e21a0ab0d851cfd95b339b41ca
jer3m01 <jer3m01@jer3m01.com>
2020-11-22 22:20:42 +0100

    feat: add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,40 @@
 module.exports = {
-  purge: [
-      './resources/js/**/*.tsx'
-  ],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {
-        height: {
-            "min-content": "min-content"
-        },
-        fontFamily: {
-            'inter': ['Inter', 'sans-serif'],
-            'mono': ['JetBrains Mono', 'sans-serif'],
+    purge: {
+        content: ['./resources/js/**/*.tsx'],
+
+        // These options are passed through directly to PurgeCSS
+        options: {
+            safelist: [
+                'row-span-1',
+                'row-span-2',
+                'row-span-3',
+                'row-span-4',
+                'row-span-5',
+                'col-span-1',
+                'col-span-2',
+                'col-span-3',
+                'col-span-4',
+                'col-span-5',
+            ],
         }
     },
-  },
-  variants: {
-    extend: {
-        display: ['dark'],
-        rotate: ['hover']
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {
+            height: {
+                "min-content": "min-content"
+            },
+            fontFamily: {
+                'inter': ['Inter', 'sans-serif'],
+                'mono': ['JetBrains Mono', 'sans-serif'],
+            }
+        },
     },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            display: ['dark'],
+            rotate: ['hover']
+        },
+    },
+    plugins: [],
 }
